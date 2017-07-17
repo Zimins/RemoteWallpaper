ac9a07915dbc23d4d89481d90bdad33557c522d8
지민 <wlals822@naver.com>
2017-07-17 13:18:33 +0900

    auth ui applied

--- app/src/main/java/com/zimincom/messagewallpaper/activities/MainActivity.java
+++ app/src/main/java/com/zimincom/messagewallpaper/activities/MainActivity.java
@@ -13,12 +13,20 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
 import com.zimincom.messagewallpaper.R;
+
+import java.util.Arrays;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,16 +35,23 @@ import static com.zimincom.messagewallpaper.R.id.imageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static int REQUEST_LOAD_IMAGE = 100;
-    private static int REQUEST_READ_STORAGE = 200;
+    private static final int RC_SIGN_IN = 11;
+    private static final int REQUEST_LOAD_IMAGE = 100;
+    private static final int REQUEST_READ_STORAGE = 200;
 
     Context context = this;
 
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseStorage firebaseStorage;
 
     // TODO: 2017. 7. 1. image notify when file uploaded
     // TODO: 2017. 7. 10. config firebase 
 
     @BindView(R.id.loadImageButton) Button loadImageButton;
+    @BindView(R.id.logout) Button logoutButton;
     @BindView(imageView) ImageView loadedImage;
 
     @Override
@@ -45,6 +60,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
 //        WallpaperManager wallpaperManager = WallpaperManager.getInstance(context);
 //
 //        try {
@@ -52,10 +70,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
 
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d("main","user logined");
+                    // TODO: 2017. 7. 13. load some state data
+                } else {
+                    Log.d("main", "user is null");
+                    startActivityForResult(AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(false)
+                            .setAvailableProviders(
+                                    Arrays.asList(
+                                            new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()
+                                    ))
+                            .build(),RC_SIGN_IN);
+                }
+            }
+        };
         loadImageButton.setOnClickListener(this);
+        logoutButton.setOnClickListener(this);
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (authStateListener != null) {
+            firebaseAuth.removeAuthStateListener(authStateListener);
+        }
+    }
+
+
+    //    private void onSignedInInitialized(String displayName) {
+//        mUsername = displayName;
+//        attachDataBaseListener();
+//    }
+//
+//    private void onSignedOutCleanUp() {
+//        mUsername = ANONYMOUS;
+//        mMessageAdapter.clear();
+//        detachDataBaseListener();
+//    }
 
     private void loadImageFromGallery() {
 
@@ -81,6 +147,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.loadImageButton:
                 loadImageFromGallery();
                 break;
+            case R.id.logout:
+                AuthUI.getInstance().signOut(this);
+                break;
         }
     }
 
@@ -88,23 +157,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            if (requestCode == REQUEST_LOAD_IMAGE) {
-                // TODO: 2017. 7. 2. check READ_EXTERNAL_STORAGE
 
-                Uri selectedImage = data.getData();
-                String[] filePathColumn = {MediaStore.Images.Media.DATA};
-
-                Cursor cursor = getContentResolver().query(selectedImage,
-                        filePathColumn, null, null, null);
-                cursor.moveToFirst();
-
-                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                String picturePath = cursor.getString(columnIndex);
-                cursor.close();
-
-                loadedImage.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(context, "welcome", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(context, "login canceled", Toast.LENGTH_SHORT).show();
             }
+        }
+
+        if (requestCode == REQUEST_LOAD_IMAGE) {
+            // TODO: 2017. 7. 2. check READ_EXTERNAL_STORAGE
+
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            loadedImage.setImageBitmap(BitmapFactory.decodeFile(picturePath));
         }
     }
 
